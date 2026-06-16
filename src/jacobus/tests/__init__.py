65a61bcfb8e918161d539ecea1ca5431e2d664c7
Johannes <johannes.programming@gmail.com>
2026-06-17 00:55:10 +0200

    Version 2.0.8

--- src/jacobus/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-def test() -> unittest.TextTestResult:
-    loader = unittest.TestLoader()
-    tests = loader.discover(start_dir="jacobus.tests")
-    runner = unittest.TextTestRunner()
-    result = runner.run(tests)
-    return result
